fc609719bf952e58af7110e6a476b72024c4a35c
Peter Kim <peterckim96@gmail.com>
2018-07-15 18:01:29 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+  attr_reader :name, :weight, :height
+  
+  @@all = []
+  
+  def initialize(pirate_hash)
+    @name = pirate_hash[:name]
+    @weight = pirate_hash[:weight]
+    @height = pirate_hash[:height]
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  attr_reader :name, :type, :booty
+  
+  @@all = []
+  
+  def initialize(ship_hash)
+    @name = ship_hash[:name]
+    @type = ship_hash[:type]
+    @booty = ship_hash[:booty]
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear
+    @@all.clear
+  end
 end
